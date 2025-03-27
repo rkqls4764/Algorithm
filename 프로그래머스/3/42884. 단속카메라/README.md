8f0dc65876b6650a6cdd46cd11991ca2daec0824
gaabeen <62369558+rkqls4764@users.noreply.github.com>
2025-03-27 22:29:47 +0900

    [level 3] Title: 단속카메라, Time: 13.01 ms, Memory: 59.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 55.8 MB, 시간: 15.63 ms
+메모리: 59.1 MB, 시간: 13.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 10:12:16
+2025년 03월 27일 22:29:45
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,51 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        boolean[] isVisit = new boolean[routes.length];
+        boolean[] isVisited = new boolean[routes.length];
         int answer = 0;
         
-        /*
-        routes 순회하면서
-        start, end를 현재로 지정
-        다음 routes를 순회하면서
-        현재 start, end에 안 겹치면 패스
-        겹치면 start, end 수정 (겹쳐지는 부분으로)
-        이번에 본 routes 방문 체크
-        */
-        
-        Arrays.sort(routes, (a, b) -> {
-            if (a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]);
-            }
+        Arrays.sort(routes, (a, b) -> { // 1. 진입점 오름차순 정렬, 2. 나간점 오름차순 정렬
+            // if (a[0] == b[0]) {
+            //     return Integer.compare(a[1], b[1]);
+            // }
             return Integer.compare(a[0], b[0]);
         });
         
         for (int i = 0; i < routes.length; i++) {
-            if (isVisit[i]) {
-                // System.out.println(i + "번 패스");
+            if (isVisited[i]) {
                 continue;
             }
             
             int start = routes[i][0];
             int end = routes[i][1];
-            isVisit[i] = true;
-            // System.out.println(i + "번 사용(" + start + ", " + end + ")");
+            isVisited[i] = true;
             
             for (int j = i + 1; j < routes.length; j++) {
-                if (isVisit[j]) {
-                    // System.out.println(j + "번 방문했음");
+                if (isVisited[j]) {
                     continue;
                 }
                 
                 if (routes[j][1] < start || end < routes[j][0]) { // 방문했거나 범위 밖이면 종료
-                    // System.out.println(j + "번 범위 밖임");
                     continue;
                 }
                 
                 start = Math.max(start, routes[j][0]);
                 end = Math.min(end, routes[j][1]);
-                isVisit[j] = true;
-                // System.out.println(j + "번으로 갱신(" + start + ", " + end + ")");
+                isVisited[j] = true;
             }
             
             answer++;
